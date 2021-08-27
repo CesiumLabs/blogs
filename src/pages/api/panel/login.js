a5c7735aab5c1b5c380152a8c1ed91a332efb528
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-27 21:06:42 +0530

    fix: removed state query from login redirection url

--- src/pages/api/panel/login.js
+++ src/pages/api/panel/login.js
@@ -1,3 +1,3 @@
 export default (req, res) => {
-    res.redirect(`https://discord.com/api/oauth2/authorize?response_type=code&client_id=${process.env.DISCORD_CLIENT_ID}&scope=identify&state=&redirect_uri=${encodeURIComponent(`${process.env.ORIGIN}/authorize/callback`)}&prompt=consent`);
+    res.redirect(`https://discord.com/api/oauth2/authorize?response_type=code&client_id=${process.env.DISCORD_CLIENT_ID}&scope=identify&redirect_uri=${encodeURIComponent(`${process.env.ORIGIN}/authorize/callback`)}&prompt=consent`);
 }
