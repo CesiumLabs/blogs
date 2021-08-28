da3433e3915e801d67f5b0d1dc212f96f531a6b8
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-28 19:18:39 +0530

    refactor: mobile responsive for member ui

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,5 @@
 export function SocialButton({ svg, href, color, children }) {
-    return <a href={href} className={`ml-1 mt-4 inline-block text-white rounded-default px-4 py-2 font-bold cursor-pointer ${color}`}>
+    return <a href={href} className={`ml-1 mt-1 inline-block text-white rounded-default px-4 py-2 font-bold cursor-pointer ${color}`}>
         <i className={svg}/> {children}
     </a>
 }
