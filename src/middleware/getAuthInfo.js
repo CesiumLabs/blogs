9da7e301b624ee165303a83d130e7cbd58eed955
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-28 09:49:54 +0530

    fix: getAuthInfo

--- src/middleware/getAuthInfo.js
+++ src/middleware/getAuthInfo.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 import cookie from 'cookie';
+import { defaultCookieOptions } from '../utils';
 
-export default ({ req, res }) => {
+export default async ({ req, res }) => {
+    res.redirect = x => res.writeHead(302, { Location: x }).end();
     const { auth_id, rank } = cookie.parse(req.headers.cookie || {});
     if (!auth_id) return res.redirect('/api/panel/login');
     
-    const { data } = await axios.get('https://backend.snowflakedev.org/api/authorize', { 
-        headers: { authorization: `Bearer ${auth_id}` } 
-    });
+    try {
+        const { data } = await axios.get('https://backend.snowflakedev.org/api/authorize', { 
+            headers: { authorization: `Bearer ${auth_id}` } 
+        });
 
-    return { user: data, rank };
+        return { user: data, rank };
+    } catch(e) {
+        res.setHeader('Set-Cookie', [cookie.serialize('auth_id', '', defaultCookieOptions), cookie.serialize('rank', 0, defaultCookieOptions)]);
+        return res.redirect('/api/panel/login');
+    }
 }
