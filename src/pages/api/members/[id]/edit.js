94e2ce55e69499c3fbbf90f2e7ca7b5184d5a360
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-28 18:44:14 +0530

    fix: member edit api

--- src/pages/api/members/[id]/edit.js
+++ src/pages/api/members/[id]/edit.js
@@ -3,13 +3,13 @@ import { User } from '../../../../utils/schemas';
 const headerKeys = ['bio', 'twitter', 'github', 'website'];
 
 export default async (req, res) => {
-    if (req.method == "POST") {
+    if (req.method == "POST" && req.headers.origin == process.env.ORIGIN) {
         const newUser = {};
         for (const key of headerKeys)
             if (req.headers[key])
                 newUser[key] = req.headers[key];
         
         await User.findOneAndUpdate({ id: req.query.id }, newUser);
-        res.status(200).json({});
+        res.status(204).end();
     } else res.status(404).json({})
 }
