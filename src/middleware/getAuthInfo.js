d66bab1753db4eca44d78cdd4c45ab0c59a0cde0
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-28 09:56:44 +0530

    fix: fix props in panel

--- src/middleware/getAuthInfo.js
+++ src/middleware/getAuthInfo.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import cookie from 'cookie';
 import { defaultCookieOptions } from '../utils';
 
-export default async ({ req, res }) => {
+export default async function getAuthInfo({ req, res }) {
     res.redirect = x => res.writeHead(302, { Location: x }).end();
     const { auth_id, rank } = cookie.parse(req.headers.cookie || {});
     if (!auth_id) return res.redirect('/api/panel/login');
@@ -12,7 +12,7 @@ export default async ({ req, res }) => {
             headers: { authorization: `Bearer ${auth_id}` } 
         });
 
-        return { user: data, rank };
+        return { user: data.data, rank };
     } catch(e) {
         res.setHeader('Set-Cookie', [cookie.serialize('auth_id', '', defaultCookieOptions), cookie.serialize('rank', 0, defaultCookieOptions)]);
         return res.redirect('/api/panel/login');
--- src/pages/panel.js
+++ src/pages/panel.js
@@ -2,18 +2,20 @@ import Frame from '../components/frame';
 import getAuthInfo from '../middleware/getAuthInfo';
 import connectMongoose from '../middleware/mongodb';
 
-export default function Panel({ forbidden, avatarURL, username }) {
+export default function Panel({ forbidden, user }) {
     if (forbidden) return 'You are not allowed to use the panel...';
     
     return <Frame title="Panel" description="Staff panel for admins and developers of the origanization">
         <div className="p-10">
             <div className="bg-theme-100 rounded-lg" style={{ padding: '1rem' }}>
                 <div className="md:flex flex-wrap">
-                    <img className="w-300 max-w-lg:w-full rounded-lg block" src={avatarURL + '?size=2048'} draggable="false" alt={username}/>
+                    <img className="max-w-lg:w-full rounded-lg block" src={user.avatarURL + '?size=2048'} draggable="false" alt={user.username}/>
                 </div>
             </div>
         </div>
     </Frame>
 }
 
-Panel.getInitialProps = getAuthInfo;
+Panel.getInitialProps = async (ctx) => {
+    return getAuthInfo(ctx);
+}
