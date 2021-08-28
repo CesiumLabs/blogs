552744884cc1f325039ec5600bd75385dabd9fd0
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-28 11:39:37 +0530

    feat: design panel

--- src/pages/panel.js
+++ src/pages/panel.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Frame from '../components/frame';
 import getAuthInfo from '../middleware/getAuthInfo';
 import connectMongoose from '../middleware/mongodb';
@@ -7,10 +8,14 @@ const BADGES = [null, { name: 'ADMIN', color: 'bg-red-800' }, { name: 'DEVELOPER
 
 export default function Panel({ forbidden, user, rank, articles }) {
     if (forbidden) return 'You are not allowed to use the panel...';
+
     const badge = BADGES[rank];
+    useEffect(() => {
+        (document.body || document.documentElement).style.backgroundColor = '#253347';
+    });
     
     return <Frame title="Panel" description="Staff panel for admins and developers of the origanization">
-        <div className="p-10">
+        <div className="p-4 md:p-10">
             <div className="bg-theme-100 rounded-lg" style={{ padding: '1rem' }}>
                 <div className="md:flex flex-wrap">
                     <img className="md:w-300 rounded-xl block" src={user.avatarURL + '?size=1024'} draggable="false" alt={user.username}/>
