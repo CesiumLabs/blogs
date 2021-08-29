b0ac395df85f33918021d0af7e90d70ac0a1a697
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-29 16:07:07 +0530

    feat: update new page

---
+++ src/components/input.jsx
@@ -0,0 +1,13 @@
+export default function Input({ name, description, placeholder, id }) {
+    return (
+        <tr>
+            <td className="w-1/4">
+                <p className="text-xl font-bold">{name}</p>
+                <p className="opacity-75 mb-3">{description}</p>
+            </td>
+            <td>
+                <input className="ml-2 px-2 py-1 rounded-default outline-none text-black block w-full" placeholder={placeholder} id={id}/>
+            </td>
+        </tr>
+    )
+}
--- src/pages/new.js
+++ src/pages/new.js
@@ -1,11 +1,22 @@
 import Frame from "../components/frame";
+import Input from "../components/input";
 import { getAuthID } from '../utils';
 
 export default function New() {
     return (
         <Frame title="New" description="Add new blog.">
             <div className="p-4 md:p-10">
-                <div className="bg-theme-100 rounded-lg p-8"></div>
+                <div className="bg-theme-100 rounded-lg p-8">
+                    <h1 className="text-white text-5xl font-bold">New Blog</h1>
+                    <p className="opacity-75 text-white">Remember to make your blog perfect, attractive and useful!</p>
+
+                    <table className="text-white w-full mt-2">
+                        <Input name="Name" description="The name of the blog. Required." placeholder="Awesome blog." id="name_input"/>
+                        <Input name="Description" description="The short description of your blog. Required." placeholder="My awesome blog." id="dsc_input"/>
+                        <Input name="Tags" description="The tags for the blog. Maximum 5 tags. Seperated by comma." placeholder="tech, javascript, web" id="tags_input"/>
+                        <Input name="Thumbnail" description="The thumbnail url of the flag. Required." placeholder="https://example.com/image.png" id="thumbnail_input"/>
+                    </table>
+                </div>
             </div>
         </Frame>
     );
