d049cadfcc4370bab6eb6ff93b6ca4a9e027cd0d
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-29 16:09:27 +0530

    fix: update settings ui

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -23,7 +23,10 @@ export default function Settings() {
         <Frame title="Settings" description="Settings for the snowflake blog website's user interface.">
             <div className="p-4 md:p-10">
                 <div className="bg-theme-100 rounded-lg p-8">
-                    <table className="text-white w-full">
+                <h1 className="text-white text-5xl font-bold">Settings</h1>
+                    <p className="opacity-75 text-white">Settings for the snowflake blog website's user interface.</p>
+
+                    <table className="text-white w-full mt-2">
                         <tr>
                             <td className="w-1/4">
                                 <p className="text-xl font-bold">Background Color</p>
