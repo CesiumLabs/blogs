8a3a35e1eaa67a5342416082c02e0286d0b9284e
Andromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-30 14:22:59 +0545

    refactor: change backend url

--- src/middleware/getAuthInfo.js
+++ src/middleware/getAuthInfo.js
@@ -7,7 +7,7 @@ export default async function getAuthInfo({ req, res }) {
     if (!auth_id) return null
 
     try {
-        const { data } = await axios.get("https://backend.snowflakedev.org/api/authorize", { headers: { authorization: `Bearer ${auth_id}` } });
+        const { data } = await axios.get("https://backend.snowflakedev.cf/api/authorize", { headers: { authorization: `Bearer ${auth_id}` } });
         return data.data;
     } catch (e) {
         res.setHeader("Set-Cookie", [cookie.serialize("auth_id", "", defaultCookieOptions), cookie.serialize("rank", 0, defaultCookieOptions)]);
@@ -20,7 +20,7 @@ export async function getAuthInfoAPI(req) {
     if (!auth_id) return null;
 
     try {
-        const { data } = await axios.get("https://backend.snowflakedev.org/api/authorize", { headers: { authorization: `Bearer ${auth_id}` } });
+        const { data } = await axios.get("https://backend.snowflakedev.cf/api/authorize", { headers: { authorization: `Bearer ${auth_id}` } });
         return data.data;
     } catch (e) {
         return null;
