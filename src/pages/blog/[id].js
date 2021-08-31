8270806d915a5ae11e66aac4078eb8785e82b3a0
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-31 16:46:25 +0530

    refactor: update blog page ui

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,28 +1,47 @@
+import { useRef, useEffect } from "react";
 import moment from "moment";
 import mongoose from "mongoose";
 import connectMongoose from "../../middleware/mongodb";
 import { Blog } from "../../utils/schemas";
+import renderMarkdown from "../../utils/markdown";
 import Frame from "../../components/frame";
+import "highlight.js/styles/agate.css";
 
-export default function BlogPage({ name, description, thumbnail, tags, author, createdAt, content }) {
+export default function BlogPage({ name, description, thumbnail, tags, author, createdAt, updatedAt, content }) {
+    const mainContentElement = useRef(null);
+    useEffect(() => window.addEventListener('load', () => mainContentElement.current.innerHTML = renderMarkdown(content)))
+
     return <Frame title={name} description={description} image={thumbnail}>
         <div className="p-8 w-full">
-            <div className="p-6 shadow-md rounded-lg bg-white w-full border-default border-grey-100">
-                <h1 className="text-8xl font-bold">{name}</h1>
-                <div className="mt-1">
-                    {(tags.length ? tags : ['nil']).map((x, i) => <a className={`font-consolas text-white rounded-sm py-1 px-2 bg-teal-600 hover:bg-teal-500 uppercase font-bold ${i == 0 ? "" : "ml-2"}`} href={`/?tag=${encodeURIComponent(x)}`}>{x}</a>)}
-                </div>
-
-                <p className="opacity-80 block mt-1 mb-1 font-changa text-lg">{description}</p>
-
-                {author ? (
-                    <div>
-                        <img className="w-5 rounded-full inline-block mr-2 -mt-1" src={author.avatar} alt={author.username}/>
-                        <p className="opacity-80 leading-none inline-block">
-                            <a className="font-bold hover:underline hover:opacity-100" href={`/member/${author.id}`}>{author.username}</a>・{moment(createdAt).fromNow()}・{getReadTime(content)}
-                        </p>
+            <div className="shadow-2md rounded-lg">
+                <div className="p-6 rounded-t-lg w-full text-white" style={{
+                    backgroundImage: `url(${thumbnail})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundBlendMode: 'multiply',
+                    backgroundColor: '#303045',
+                    backgroundPosition: 'center'
+                }}>
+                    <h1 className="text-8xl font-bold">{name}</h1>
+                    <div className="mt-1">
+                        {(tags.length ? tags : ['nil']).map((x, i) => <a className={`font-consolas text-white rounded-sm py-1 px-2 bg-teal-600 hover:bg-teal-500 uppercase font-bold ${i == 0 ? "" : "ml-2"}`} href={`/?tag=${encodeURIComponent(x)}`}>{x}</a>)}
                     </div>
-                ) : null}
+
+                    <p className="opacity-80 block mt-1 mb-1 font-changa text-lg">{description}</p>
+
+                    {author ? (
+                        <div>
+                            <img className="w-5 rounded-full inline-block mr-2 -mt-1" src={author.avatar} alt={author.username}/>
+                            <p className="opacity-80 leading-none inline-block">
+                                <a className="font-bold hover:underline hover:opacity-100" href={`/member/${author.id}`}>{author.username}</a>・{moment(createdAt).fromNow()}・{getReadTime(content)}
+                            </p>
+                        </div>
+                    ) : null}
+                </div>
+                <div className="p-6 shadow-md rounded-b-lg bg-white w-full border-default border-grey-100">
+                    <div className="mt-2" ref={mainContentElement}/>
+                    <p className="mt-2 text-lg">Last updated {moment(updatedAt).fromNow()}...</p>
+                </div>
             </div>
         </div>
     </Frame>
