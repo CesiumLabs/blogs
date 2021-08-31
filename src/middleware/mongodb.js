5f3e0b4ed45a1dbe304e8fb1d980e0bafdb0b895
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-31 11:22:54 +0530

    refactor: remove [useUnifiedTopology] option from mongoose connection

--- src/middleware/mongodb.js
+++ src/middleware/mongodb.js
@@ -11,9 +11,6 @@ export default async function connectMongoose() {
             mongoose.staffs.set(staff.id, staff);
         }
 
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
     }
 }
