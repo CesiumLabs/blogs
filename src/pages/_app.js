d8bc20747ce336c6eac4550d1dee139f519bd220
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-31 14:53:55 +0530

    feat: remove settings page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,5 @@
-import { useEffect } from "react";
 import "../assets/styles/tailwind.css";
 
 export default function App({ Component, pageProps }) {
-    useEffect(() => {
-        let customBg = localStorage.getItem("custom_bg");
-        if (customBg) (document.body || document.documentElement).style.backgroundColor = customBg;
-    }, []);
-
     return <Component {...pageProps} />;
 }
