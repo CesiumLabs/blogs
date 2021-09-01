be954088117ece8fcf881ea9285faf4302a62f69
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-09-01 22:45:05 +0530

    refactor: change mongoose connection options

--- src/middleware/mongodb.js
+++ src/middleware/mongodb.js
@@ -13,6 +13,10 @@ export default async function connectMongoose() {
 
     if (!mongoose.connection?.readyState) {
         if (!process.env.MONGO_URI) throw "No mongo url has been provided";
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            bufferCommands: false
+        });
     }
 }
