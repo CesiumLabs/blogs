d9066a97b7af76cedd1e6259be2761c766583341
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-09-01 22:48:44 +0530

    refactor: change mongoose connection options

--- src/middleware/mongodb.js
+++ src/middleware/mongodb.js
@@ -16,7 +16,8 @@ export default async function connectMongoose() {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            bufferCommands: false
+            bufferCommands: false,
+            bufferMaxEntries: 0
         });
     }
 }
