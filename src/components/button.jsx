eff3b7dc434cd3fa9f6ca77a90abc53393eee405
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-09-01 20:04:20 +0530

    fix: update button ui

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,6 +1,6 @@
 export function SocialButton({ svg, href, color, children, onClick }) {
     return (
-        <a href={href} onClick={onClick} className={`ml-1 mt-1 inline-block text-white rounded-default px-4 py-2 font-bold cursor-pointer ${color}`}>
+        <a href={href} onClick={onClick} className={`ml-1 mt-1 inline-block leading-none text-white rounded-default px-4 py-2 font-bold cursor-pointer ${color}`}>
             <i className={svg} /> {children}
         </a>
     );
