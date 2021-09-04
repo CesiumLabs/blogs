83f4705fd3c8f867114eeafc7e3a93f6f91272e9
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-09-04 19:37:41 +0530

    fix: imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { Blog } from "../utils/schemas";
 import connectMongoose from "../middleware/mongodb";
 import parseSearch from "../utils/searchParser";
 import Frame from "../components/frame";
